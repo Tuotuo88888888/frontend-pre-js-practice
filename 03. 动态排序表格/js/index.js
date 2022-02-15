8f4467c775ead0227675cad676514911d39ea1c6
YuanJin <277739025@qq.com>
2022-02-15 19:53:42 +0800

    更新了动态表格排序的资料

--- 03. 动态排序表格/js/index.js
+++ 03. 动态排序表格/js/index.js
@@ -1,6 +1,4 @@
 /* 创建事入口函数
-  0: 两部分内容是平行的 ，首先就是复选框的关联操作。
-
    1：首先为表头th添加事件绑定； 
    2：DOM查询获取所有的表头
    3：为ths注册点击事件，同时传递index值到事件函数内部
@@ -22,12 +20,12 @@
     定义统计数字 checkedNum = 0
  */
 
-(function () {
-  var ths = document.getElementsByTagName('th')   // th集合
-  var tbody = document.getElementsByTagName('tbody')[0] // tbody 获取之后，在追加元素及查找复选框的时候进行使用
+!function () {
+  var ths = document.querySelectorAll('th')   // th集合
+  var tbody = document.querySelector('tbody') // tbody 获取之后，在追加元素及查找复选框的时候进行使用
   var rows = tbody.querySelectorAll('tr')     // 所有的行的集合
-  var checkAll = document.getElementById('checkAll')   // 全选按钮获取
-  var checkOneList = tbody.getElementsByTagName('input')   // 单选按钮集合获取
+  var checkAll = document.querySelector('.checkAll')   // 全选按钮获取
+  var checkOneList = tbody.querySelectorAll('[type="checkbox"]')   // 单选按钮集合获取
 
   /* 程序入口函数 */
   var init = function () {
@@ -36,61 +34,54 @@
 
   /* 事件入口函数 */
   var initEvent = function () {
-    for (var i = 0; i < ths.length; i++) {
-      var node = ths[i]
-      onThsClick(node, i);
-      // node.addEventListener('click', onThsClick.bind(node, index))
-    }
+    ths.forEach(function (node, index) {
+      node.addEventListener('click', onThsClick.bind(node, index))
+    })
     // 全选按钮处理
     checkAll.addEventListener('click', onCheckAllClick)
     // 单选按钮事件绑定
-    tbody.addEventListener('click', onCheckOneClick)
-    // checkOneList.forEach(function (node) {
-    //   node.addEventListener('click', onCheckOneClick)
-    // })
+    checkOneList.forEach(function (node) {
+      node.addEventListener('click', onCheckOneClick)
+    })
+
   }
 
-  /* 全部选中操作函数 */
+  /* 全部选中 */
   var onCheckAllClick = function (e) {
     e.stopPropagation()
     var checkStatus = this.checked
-    for (let i = 0; i < checkOneList.length; i++) {
-      const ele = checkOneList[i];
-      ele.checked = checkStatus
-    }
+    checkOneList.forEach(function (node) {
+      node.checked = checkStatus
+    })
   }
 
   /* 单个进行选中 */
-  var onCheckOneClick = function (e) {
-    if (e.target.tagName !== 'INPUT') return
+  var onCheckOneClick = function () {
     /* 定义统计数字 */
     var checkedNum = 0
-
-    for (var i = 0; i < checkOneList.length; i++) {
-      checkOneList[i].checked && ++checkedNum
-    }
+    checkOneList.forEach(function (node) {
+      node.checked && ++checkedNum
+    })
     checkAll.checked = checkedNum === checkOneList.length
   }
 
   /* ths表头事件绑定事件绑定 */
-  var onThsClick = function (node, index) {
-    if (index === 0) return
+  var onThsClick = function (index) {
     /* 处理索引值为1复选框的时候的问题 */
-    node.addEventListener('click', function () {
-      var arr = Array.prototype.slice.apply(rows).sort(function (a, b) {
-        if (index === 2 || index === 4) {
-          /* localeCompare是一种基于国际化字体的地区字符比较，例如中国用中文，美国用英文，法国用法文，德国用德文。。将这些国家的文字按照国家/地区等进行编号，然后每个编号都对应了该国/地区的文字。 */
-          return a.getElementsByTagName('td')[index].innerHTML.localeCompare(b.getElementsByTagName('td')[index].innerHTML, 'zh')
-        }
-        return a.getElementsByTagName('td')[index].innerHTML - b.getElementsByTagName('td')[index].innerHTML
-      })
-      arr.forEach(function (node) {
-        tbody.appendChild(node)
-      })
+    if (index === 0) return
+    var arr = Array.prototype.slice.apply(rows).sort(function (a, b) {
+      if (index === 2 || index === 4) {
+        /* localeCompare是一种基于国际化字体的地区字符比较，例如中国用中文，美国用英文，法国用法文，德国用德文。。将这些国家的文字按照国家/地区等进行编号，然后每个编号都对应了该国/地区的文字。 */
+        return a.querySelectorAll('td')[index].innerHTML.localeCompare(b.querySelectorAll('td')[index].innerHTML, 'zh')
+      }
+      return a.querySelectorAll('td')[index].innerHTML - b.querySelectorAll('td')[index].innerHTML
+    })
+    arr.forEach(function (node) {
+      tbody.appendChild(node)
     })
   }
 
   /* Node.appendChild() 方法将一个节点附加到指定父节点的子节点列表的末尾处。如果将被插入的节点已经存在于当前文档的文档树中，那么 appendChild() 只会将它从原先的位置移动到新的位置（不需要事先移除要移动的节点）。
    */
   init()
-})();
+}();
