73ea3e24de0cd595bf905d35e0e60fce2004e704
tuotuo88888888 <2646218690@qq.com>
2023-03-08 21:53:04 +0800

    13++

--- 13. 读心术小游戏/自己写/index.js
+++ 13. 读心术小游戏/自己写/index.js
@@ -1,18 +1,17 @@
 var doms = {
   chartletList: document.querySelector(".chartlet-list"),
   gamebtn: document.querySelector(".gamebtn"),
+  gameCentrePoint: document.querySelector(".game-centre-point"),
   gameContent: document.querySelector(".game-content"),
 };
 var chartletImgCount = 16;
 var specialChartlet;
+var isGameOver = false;
 function main() {
   initThought();
+  bindEvents();
 }
 function initThought() {
-  initGameBtn();
-  initChartlet();
-}
-function initChartlet() {
   specialChartlet = Math.floor(Math.random() * chartletImgCount);
   doms.chartletList.innerHTML = "";
   var chartletListContext = document.createDocumentFragment();
@@ -22,6 +21,7 @@ function initChartlet() {
       random = specialChartlet;
     }
     var itemDiv = document.createElement("div");
+    itemDiv.className = "item";
     var itemNo = document.createElement("span");
     var itemImg = document.createElement("img");
     itemNo.innerText = i;
@@ -30,8 +30,32 @@ function initChartlet() {
     chartletListContext.appendChild(itemDiv);
   }
   doms.chartletList.appendChild(chartletListContext);
+  doms.gameContent.src = `./../images/values/${specialChartlet}.png`;
+  doms.gameContent.style.opacity = 0;
+  doms.gameCentrePoint.style.opacity = 1;
 }
-function initGameBtn() {
-  doms.gameContent.src = "./../images/round.png";
+function bindEvents() {
+  doms.gamebtn.addEventListener("click", game);
+}
+function game() {
+  if (!isGameOver) {
+    doms.gamebtn.removeEventListener("click", game);
+    doms.gamebtn.style.transition = "all 2s ease-out";
+    doms.gamebtn.style.transform = "rotate(1800deg)";
+    doms.gamebtn.addEventListener("transitionend", function () {
+      doms.gamebtn.style.transition = "none";
+      doms.gameCentrePoint.style.opacity = 0;
+      doms.gameContent.style.opacity = 1;
+      this.removeEventListener("transitionend", arguments.callee);
+      doms.gamebtn.addEventListener("click", game);
+    });
+    isGameOver = true;
+  } else {
+    if (window.confirm("你确定要继续吗？")) {
+      doms.gamebtn.style = "";
+      initThought();
+      isGameOver = false;
+    }
+  }
 }
 main();
